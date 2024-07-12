5ab0651688f09af8dcb13a6eb16a70ea16f71938
Subinoy Biswas <heysubinoy@gmail.com>
2024-07-12 13:25:33 +0530

    add: fetch transformed images

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,10 @@ import cors from 'cors';
 import { normalizer } from "./helpers/normalizer"
 import aws from 'aws-sdk';
 import dotenv from "dotenv";
+import Sharp from 'sharp';
 
 dotenv.config();
-aws.config.update({ region: process.env.AWS_REGION });
+aws.config.update({ region: process.env.AWS_REGION, correctClockSkew: true });
 
 const s3 = new aws.S3();
 
@@ -33,8 +34,8 @@ app.get('*', async (req, res) => {
     Bucket: originalBucket,
     Key: sendObj.pathname.substring(1) as string
   };
-  console.log("Params", params);
-  console.log("SendObj", sendObj);
+  // console.log("Params", params);
+  // console.log("SendObj", sendObj);
   if (!sendObj.format && !sendObj.width && !sendObj.height && !sendObj.quality) {
     try {
       s3.getObject(params, (err, data) => {
@@ -51,7 +52,64 @@ app.get('*', async (req, res) => {
       return res.status(500).json({ error: "Server Error" });
     }
   } else {
-    return res.status(200).json(sendObj);
+    //Check if formatted exists
+    //Format Image Upload to S3
+
+    const formattedBucket = process.env.S3_FORMATTED_IMAGE_BUCKET as string;
+    let formattedName = sendObj.pathname.substring(1).split('.')[0];
+    if (sendObj.format) {
+      formattedName += `.${sendObj.format}`;
+    } else {
+      formattedName += `${sendObj.pathname.substring(1).split('.')[1]}`;
+    }
+    if (sendObj.width) {
+      formattedName += `_${sendObj.width}w`;
+    }
+    if (sendObj.height) {
+      formattedName += `_${sendObj.height}h`;
+    }
+    if (sendObj.quality) {
+      formattedName += `_${sendObj.quality}q`;
+    }
+
+    console.log(formattedName)
+
+    const params = {
+      Bucket: formattedBucket,
+      Key: formattedName
+    };
+    // console.log(sendObj)
+    try {
+      s3.getObject(params, (err, data) => {
+        if (err) {
+          if (err && err.code === 'NoSuchKey') {
+
+            s3.getObject(params, (err, data) => {
+              if (err) {
+                console.error(err);
+                return res.status(400).json({ error: "Original file doesn't Exist" });
+              } else {
+                let imageBody = data.Body;
+                if (imageBody) {
+                  imageBody = imageBody;
+                }
+
+
+              }
+            });
+
+          }
+          console.error(err);
+          return res.status(500).json({ error: "Error Occured" });
+        } else {
+          console.log('File downloaded successfully');
+          return res.send(data.Body).status(200);
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Server Error" });
+    }
   }
 });
 
